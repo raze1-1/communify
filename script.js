9a6ecabfa07a2545c159549f7452bf8a715ccab0
raze1-1 <136592924+raze1-1@users.noreply.github.com>
2023-06-14 18:37:11 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement(
+    {
+      pageLanguage: 'en',
+      includedLanguages: 'en,ar,es,fr,de,it,ja,ko,pt,ru,zh-CN',
+      layout: google.translate.TranslateElement.InlineLayout.SIMPLE
+    },
+    'google_translate_element'
+  );
+}
 
